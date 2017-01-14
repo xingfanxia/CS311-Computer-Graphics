c1e29653198efb5936e50ab1f1a07338cfce6d8d
xxf1995 <xiax@carleton.edu>
2017-01-13 18:24:31 -0600

    advance to full vector struc

--- 040 TextureMapping/040triangle.c
+++ 040 TextureMapping/040triangle.c
@@ -3,7 +3,7 @@
 
 
 //To Generate Color at vector x by applying formulae learnt in class
-void getSTcoordinates(double x0, double x1, double a[2], double b[2], double c[2], double rgb[3],
+void getSTcoordinates(double x[2], double a[2], double b[2], double c[2], double rgb[3],
 		double alpha[2], double beta[2], double gamma[2], double coordinates[2]) {
 
 	double m[2][2] = {
@@ -15,7 +15,7 @@ void getSTcoordinates(double x0, double x1, double a[2], double b[2], double c[2
 		{0.0,0.0}
 	};	
 	double det = mat22Invert(m,Inv);
-	double vec[2] = {x0-a[0], x1-a[1]};
+	double vec[2] = {x[0]-a[0], x[1]-a[1]};
 	double pq[2] = {0.0, 0.0};
 	mat221Multiply(Inv,vec,pq);
 	// vectorPp(pq);
@@ -33,47 +33,39 @@ using the formulae we learnt in class.*/
 void triRenderALeft(double a[2], double b[2], double c[2], double rgb[3], texTexture *tex, 
 		double alpha[2], double beta[2], double gamma[2]) {
 	
-	double a0 = a[0];
-	double a1 = a[1];
-	double b0 = b[0];
-	double b1 = b[1];
-	double c0 = c[0];
-	double c1 = c[1];
 	double STvalue[2];
 	double *sampleRGB;
-
-	//c0<b0, so it is an Acute Triangle/Right Triangle, Angle(abc)<=90
-	if (c0 <= b0){ 
+	double x[2];
+	//c[0]<b[0], so it is an Acute Triangle/Right Triangle, Angle(abc)<=90
+	if (c[0] <= b[0]){ 
 
 		//A and C on the same y-axis
-		if (a0 == c0){ 
+		if (a[0] == c[0]){ 
 			/* Some triangle
 			C
 
 						B
 			A            
 			*/
-			int x0;
-			/* The left bound is (int)ceil(a0) and the right bound is (int)floor(b0)*/
-			for (x0=(int)ceil(a0); x0<=(int)floor(b0); x0++){
+			/* The left bound is (int)ceil(a[0]) and the right bound is (int)floor(b[0])*/
+			for (x[0]=(int)ceil(a[0]); x[0]<=(int)floor(b[0]); x[0]++){
 				int x1_low;
 				int x1_high;
 				/*The lower bound is ac and the higher bound is bc*/
-				x1_low = a1+(b1-a1)/(b0-a0)*(x0-a0);
-				x1_high = c1+(b1-c1)/(b0-c0)*(x0-c0);
-				int x1;
-				for (x1=(int)ceil(x1_low); x1<=(int)floor(x1_high); x1++){
-					getSTcoordinates(x0, x1, a, b, c, rgb, alpha, beta, gamma, STvalue);
+				x1_low = a[1]+(b[1]-a[1])/(b[0]-a[0])*(x[0]-a[0]);
+				x1_high = c[1]+(b[1]-c[1])/(b[0]-c[0])*(x[0]-c[0]);
+				for (x[1]=(int)ceil(x1_low); x[1]<=(int)floor(x1_high); x[1]++){
+					getSTcoordinates(x, a, b, c, rgb, alpha, beta, gamma, STvalue);
 					texSample(tex, STvalue[0], STvalue[1]);
 					sampleRGB = tex->sample;
 					sampleRGB[0] *= rgb[0];
 					sampleRGB[1] *= rgb[1];
 					sampleRGB[2] *= rgb[2];
-					pixSetRGB(x0,x1,sampleRGB[0],sampleRGB[1],sampleRGB[2]);
+					pixSetRGB(x[0], x[1], sampleRGB[0], sampleRGB[1], sampleRGB[2]);
 				}
 			}
 		/*case2: B and C on the same y-axis*/
-		} else if (c0 == b0){
+		} else if (c[0] == b[0]){
 			/* Some triangle
 						 C
 
@@ -81,21 +73,19 @@ void triRenderALeft(double a[2], double b[2], double c[2], double rgb[3], texTex
 						 B
 			A            
 			*/			
-			int x0;
-			for (x0=(int)ceil(a0); x0<=(int)floor(b0); x0++){
+			for (x[0]=(int)ceil(a[0]); x[0]<=(int)floor(b[0]); x[0]++){
 				int x1_low;
 				int x1_high;
-				x1_low = a1+(b1-a1)/(b0-a0)*(x0-a0);
-				x1_high = a1+(c1-a1)/(c0-a0)*(x0-a0);
-				int x1;
-				for (x1=(int)ceil(x1_low); x1<=(int)floor(x1_high); x1++){
-					getSTcoordinates(x0, x1, a, b, c, rgb, alpha, beta, gamma, STvalue);
+				x1_low = a[1]+(b[1]-a[1])/(b[0]-a[0])*(x[0]-a[0]);
+				x1_high = a[1]+(c[1]-a[1])/(c[0]-a[0])*(x[0]-a[0]);
+				for (x[1]=(int)ceil(x1_low); x[1]<=(int)floor(x1_high); x[1]++){
+					getSTcoordinates(x, a, b, c, rgb, alpha, beta, gamma, STvalue);
 					texSample(tex, STvalue[0], STvalue[1]);
 					sampleRGB = tex->sample;
 					sampleRGB[0] *= rgb[0];
 					sampleRGB[1] *= rgb[1];
 					sampleRGB[2] *= rgb[2];
-					pixSetRGB(x0,x1,sampleRGB[0],sampleRGB[1],sampleRGB[2]);
+					pixSetRGB(x[0], x[1], sampleRGB[0], sampleRGB[1], sampleRGB[2]);
 				}
 			}
 		/*case3: B in the middle of A and B in terms of x-coordinates*/
@@ -107,83 +97,77 @@ void triRenderALeft(double a[2], double b[2], double c[2], double rgb[3], texTex
 						 B
 			A            
 			*/	
-			int x0;
-			for (x0=(int)ceil(a0); x0<=(int)floor(c0); x0++){
+			for (x[0]=(int)ceil(a[0]); x[0]<=(int)floor(c[0]); x[0]++){
 				//Left sub-Triangle of Triangle ABC
 				int x1_low;
 				int x1_high;
-				x1_low = a1+(b1-a1)/(b0-a0)*(x0-a0);
-				x1_high = a1+(c1-a1)/(c0-a0)*(x0-a0);
-				int x1;
-				for (x1=(int)ceil(x1_low); x1<=(int)floor(x1_high); x1++){
-					getSTcoordinates(x0, x1, a, b, c, rgb, alpha, beta, gamma, STvalue);
+				x1_low = a[1]+(b[1]-a[1])/(b[0]-a[0])*(x[0]-a[0]);
+				x1_high = a[1]+(c[1]-a[1])/(c[0]-a[0])*(x[0]-a[0]);
+				for (x[1]=(int)ceil(x1_low); x[1]<=(int)floor(x1_high); x[1]++){
+					getSTcoordinates(x, a, b, c, rgb, alpha, beta, gamma, STvalue);
 					texSample(tex, STvalue[0], STvalue[1]);
 					sampleRGB = tex->sample;
 					sampleRGB[0] *= rgb[0];
 					sampleRGB[1] *= rgb[1];
 					sampleRGB[2] *= rgb[2];
-					pixSetRGB(x0,x1,sampleRGB[0],sampleRGB[1],sampleRGB[2]);
+					pixSetRGB(x[0], x[1], sampleRGB[0], sampleRGB[1], sampleRGB[2]);
 				}
 			}
-			for (x0=(int)floor(c0)+1; x0<=(int)floor(b0); x0++){
+			for (x[0]=(int)floor(c[0])+1; x[0]<=(int)floor(b[0]); x[0]++){
 				//Right sub-Triangle of Triangle ABC
 				int x1_low;
 				int x1_high;
-				x1_low = a1+(b1-a1)/(b0-a0)*(x0-a0);
-				x1_high = c1+(b1-c1)/(b0-c0)*(x0-c0);
-				int x1;
-				for (x1=(int)ceil(x1_low); x1<=(int)floor(x1_high); x1++){	
-					getSTcoordinates(x0, x1, a, b, c, rgb, alpha, beta, gamma, STvalue);
+				x1_low = a[1]+(b[1]-a[1])/(b[0]-a[0])*(x[0]-a[0]);
+				x1_high = c[1]+(b[1]-c[1])/(b[0]-c[0])*(x[0]-c[0]);
+				for (x[1]=(int)ceil(x1_low); x[1]<=(int)floor(x1_high); x[1]++){	
+					getSTcoordinates(x, a, b, c, rgb, alpha, beta, gamma, STvalue);
 					texSample(tex, STvalue[0], STvalue[1]);
 					sampleRGB = tex->sample;
 					sampleRGB[0] *= rgb[0];
 					sampleRGB[1] *= rgb[1];
 					sampleRGB[2] *= rgb[2];
-					pixSetRGB(x0,x1,sampleRGB[0],sampleRGB[1],sampleRGB[2]);
+					pixSetRGB(x[0], x[1], sampleRGB[0], sampleRGB[1], sampleRGB[2]);
 				}
 			}
 		}
-	/* c0 > b0, Obtuse triangles. 90<Angle(abc)*/
+	/* c[0] > b[0], Obtuse triangles. 90<Angle(abc)*/
 	} else {
-		int x0;
-		for (x0=(int)ceil(a0); x0<=(int)floor(b0); x0++){
+		for (x[0]=(int)ceil(a[0]); x[0]<=(int)floor(b[0]); x[0]++){
 			//Left sub-Triangle of Triangle ABC
 			int x1_low;
 			int x1_high;
-			x1_low = a1+(b1-a1)/(b0-a0)*(x0-a0);
-			x1_high = a1+(c1-a1)/(c0-a0)*(x0-a0);
-			int x1;
-			for (x1=(int)ceil(x1_low); x1<=(int)floor(x1_high); x1++){
-				getSTcoordinates(x0, x1, a, b, c, rgb, alpha, beta, gamma, STvalue);
+			x1_low = a[1]+(b[1]-a[1])/(b[0]-a[0])*(x[0]-a[0]);
+			x1_high = a[1]+(c[1]-a[1])/(c[0]-a[0])*(x[0]-a[0]);
+			for (x[1]=(int)ceil(x1_low); x[1]<=(int)floor(x1_high); x[1]++){
+				getSTcoordinates(x, a, b, c, rgb, alpha, beta, gamma, STvalue);
 				texSample(tex, STvalue[0], STvalue[1]);
 				sampleRGB = tex->sample;
 				sampleRGB[0] *= rgb[0];
 				sampleRGB[1] *= rgb[1];
 				sampleRGB[2] *= rgb[2];
-				pixSetRGB(x0,x1,sampleRGB[0],sampleRGB[1],sampleRGB[2]);
+				pixSetRGB(x[0], x[1], sampleRGB[0], sampleRGB[1], sampleRGB[2]);
 			}
 		}
-		for (x0=(int)floor(b0)+1; x0<=(int)floor(c0); x0++){
+		for (x[0]=(int)floor(b[0])+1; x[0]<=(int)floor(c[0]); x[0]++){
 			//Right sub-Triangle of Triangle ABC
 			int x1_low;
 			int x1_high;
-			x1_low  = b1+(c1-b1)/(c0-b0)*(x0-b0);
-			x1_high = a1+(c1-a1)/(c0-a0)*(x0-a0);
-			int x1;
-			for (x1=(int)ceil(x1_low); x1<=(int)floor(x1_high); x1++){	
-				getSTcoordinates(x0, x1, a, b, c, rgb, alpha, beta, gamma, STvalue);
+			x1_low  = b[1]+(c[1]-b[1])/(c[0]-b[0])*(x[0]-b[0]);
+			x1_high = a[1]+(c[1]-a[1])/(c[0]-a[0])*(x[0]-a[0]);
+			for (x[1]=(int)ceil(x1_low); x[1]<=(int)floor(x1_high); x[1]++){	
+				getSTcoordinates(x, a, b, c, rgb, alpha, beta, gamma, STvalue);
 				texSample(tex, STvalue[0], STvalue[1]);
 				sampleRGB = tex->sample;
 				sampleRGB[0] *= rgb[0];
 				sampleRGB[1] *= rgb[1];
 				sampleRGB[2] *= rgb[2];
-				pixSetRGB(x0,x1,sampleRGB[0],sampleRGB[1],sampleRGB[2]);
+				pixSetRGB(x[0], x[1], sampleRGB[0], sampleRGB[1], sampleRGB[2]);
 			}
 
 		}
 		
-	}	
-}
+	}
+}	
 
 /*triRender is a dummy functio that calls the triRenderAleft which
 is the actual method that does the drawing and pass the vertices in
